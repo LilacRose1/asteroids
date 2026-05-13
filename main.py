d540ec0a34435e21dcf44e454cfc7ae9fdd421bb
Vlad <dodik55vovik@gmail.com>
2026-05-13 19:02:00 -0400

    updated the main function to limit the frame rate to 60 FPS. Created new file containing CircleShape class

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from logger import log_state
 def main():
     pygame.init()
 
+    clock = pygame.time.Clock()
+
+    dt = 0
+
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
 
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -15,11 +19,15 @@ def main():
 
     while True:
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
-        pygame.display.flip()    
+        pygame.display.flip() 
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
