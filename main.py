99389e61e9a0cc82df0c1d13363d0063b8c1bbd3
Vlad <dodik55vovik@gmail.com>
2026-05-13 19:17:38 -0400

    created a player file that contains a class Player that extends Circle class. Updated the main function to draw the player (white triangle) in the middle of a screen

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
-from constants import *
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
@@ -25,9 +26,15 @@ def main():
                 return
             
         screen.fill("black")
+
+        player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+        player.draw(screen)
+
         pygame.display.flip() 
         clock.tick(60)
         dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
