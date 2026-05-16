a4fb88b9f16ac2cd331a89aaddb25531f760cdf9
Vlad <dodik55vovik@gmail.com>
2026-05-15 21:42:51 -0400

    created an asteroid and asteroidfield classes. Drew them on the screen. Now we have asteroids flying around the screen

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pygame
-from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from constants import *
 from logger import log_state
 from player import Player
-
+from asteroidfield import AsteroidField
+from asteroid import Asteroid
 def main():
     pygame.init()
 
@@ -12,18 +13,19 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
 
     Player.containers = (updatable, drawable)
 
+    AsteroidField.containers = (updatable)
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-
-    print(f"Screen width: {SCREEN_WIDTH}")
-
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    field = AsteroidField()
 
     while True:
         log_state()
